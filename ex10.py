58d4838bd645858b5fedc0125f038bc408a0627b
jerrydavidlee <0327lsm@naver.com>
2016-04-06 20:29:31 +0900

    Added files via upload

---
+++ ex10.py
@@ -0,0 +1,16 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\ mon a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t * Cat fodd
+\t * Fishies
+\t * Catnip \n\t * Grass
+'''
+
+print tabby_cat
+print("%%s : %s" % tabby_cat)
+print persian_cat
+print backslash_cat
+print fat_cat
