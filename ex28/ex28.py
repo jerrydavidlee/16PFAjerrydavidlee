4a73f811eb72d91890356740ef940d6f6369480a
CAD Client <CAD Client>
2016-05-04 20:30:28 +0900

    ex28 commit

--- ex28/ex28.py
+++ ex28/ex28.py
@@ -1,8 +1,11 @@
-True and True
-False and True
-1 == 1 and 2 == 1
-"test" == "test"
-1 ==1 and 2 == 1
+
+
+
+print(" 1. True and True = %s" % str(True and True))
+print("2. False and True = %s" % str(False and True))
+print("3. 1 == 1 and 2 == 1 = %s" %((1==1) and (2==1)))
+print(" 4. 'test' == 'test' = %s" % str('test' == 'test'))
+print(" 5. 1 ==1 and 2 == 1
 True and 1 ==1
 False and 0 != 0
 True or 1 == 1
@@ -18,3 +21,4 @@ not ("testing" == "testing" and "Zed" == "Cool Gut")
 1 ==1 and (not ("testing" == 1 or 1 == 0))
 "chunky" == "bacon" and (not (3 == 4 or 3 == 3))
 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))
+#true false 연산형이다.
